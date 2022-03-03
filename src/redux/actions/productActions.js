093f7d269469f612bda8ecadb560a9f18f30b0f1
kadiraydn <kadiraydin@suraksiyon.com>
2022-03-03 21:58:34 +0300

    react error handler

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -4,6 +4,14 @@ export function getProductsSuccess(products) {
   return { type: actionTypes.GET_PRODUCTS_SUCCESS, payload: products };
 }
 
+export function createProductsSuccess(products) {
+  return { type: actionTypes.CREATE_PRODUCT_SUCCESS, payload: products };
+}
+
+export function updateProductsSuccess(products) {
+  return { type: actionTypes.UPDATE_PRODUCT_SUCCESS, payload: products };
+}
+
 export function getProducts(categoryId) {
   return function (dispatch) {
     let url = "http://localhost:3000/products";
@@ -14,4 +22,39 @@ export function getProducts(categoryId) {
       .then((response) => response.json())
       .then((result) => dispatch(getProductsSuccess(result)));
   };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3000/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+export function saveProducts(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductsSuccess(savedProduct))
+          : dispatch(createProductsSuccess(savedProduct));
+      })
+      .catch((e) => {
+        throw e;
+      });
+  };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) response.json();
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export async function handleError(error) {
+  console.error("Bir hata oluştu.");
+  throw error;
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,15 @@ import changeCategoryReducer from "./changeCategoryReducer";
 import categoryListReducer from "./categoryListReducer";
 import productListReducer from "./productListReducer";
 import cartReducer from "./cartReducer";
+import saveProductReducer from "./saveProductReducer";
+
 
 const rootReducer = combineReducers({
   changeCategoryReducer,
   categoryListReducer,
   productListReducer,
   cartReducer,
+  saveProductReducer
 });
 
 export default rootReducer;
